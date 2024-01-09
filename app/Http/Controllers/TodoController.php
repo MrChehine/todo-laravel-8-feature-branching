7afcc1b480d8529885ed937c2c6691bcd9cc7bfd
Chehine Ammari <chehine.ammari@gmail.com>
2024-01-09 22:18:58 +0100

    feat(backend): Delete todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -31,6 +31,7 @@ class TodoController extends Controller
 
     public function destroy(Todo $todo)
     {
-
+        $todo->delete();
+        return redirect()->route("index");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [TodoController::class,'index'])->name('index');
 Route::post('/', [TodoController::class,'store'])->name('store');
 Route::patch('/{todo}', [TodoController::class,'update'])->name('update');
+Route::delete('/{todo}', [TodoController::class,'destroy'])->name('destroy');
